3f75339ac9f93addf06a36f2b485a680f14b3345
Igor Zelenin <i@zelamundo.org>
2024-07-15 01:57:49 +0200

    Adjustments in timing function

--- utils/timed.py
+++ utils/timed.py
@@ -2,7 +2,7 @@ import time
 
 
 def timestamp_to_human(timestamp):
-    human_readable_time = time.strftime('%H:%M:%S%f', time.localtime(timestamp))
+    human_readable_time = time.strftime('%H:%M:%S', time.localtime(timestamp))
     return human_readable_time
 
 
@@ -14,8 +14,8 @@ def timed_func(func):
         start_time = time.time()
         result = func(*args, **kwargs)
         end_time = time.time()
-
-        print(f"Time elapsed for {func.__name__}: {end_time - start_time}s")
+        elapsed_time = "{:.6f}".format(end_time - start_time)
+        print(f"Time elapsed for {func.__name__}: {elapsed_time}s")
 
         return result
 
@@ -37,12 +37,13 @@ def timed_hoc(func, *args, **kwargs):
         try:
             name = func.module.__class__.__name__  # Works for nn.Module objects
         except AttributeError:
-            name = 'model'  # Default name if above methods fail
+            name = 'module'  # Default name if above methods fail
 
     start_time = time.time()
     print(f"Start time for {name}: {timestamp_to_human(start_time)}")
     result = func(*args, **kwargs)
     end_time = time.time()
-    print(f"Time elapsed for {name}: {end_time - start_time}s")
+    elapsed_time = "{:.6f}".format(end_time - start_time)
+    print(f"Time elapsed for {name}: {elapsed_time}s")
 
     return result
